885bb47637c5089b0a680f8d51b40f3e83b7879a
GoldenBoy <57160141+Mateus-Felipe-Ribeiro@users.noreply.github.com>
2021-12-16 10:46:54 -0300

    Add files via upload

---
+++ SpringFootball/src/test/java/PMM/SpringFootball/SpringFootballApplicationTests.java
@@ -0,0 +1,13 @@
+package PMM.SpringFootball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
